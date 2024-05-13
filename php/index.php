d7d3d6cdf30221b4fbba495b65718c50ba9bc2cc
Fran Navarro <frnaad@gmail.com>
2024-05-13 19:16:14 +0200

    Better formats

--- php/index.php
+++ php/index.php
@@ -10,80 +10,88 @@
 
 		if (isset($_POST["login"])) {
 
-			$mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
-
-			$pass = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-
-			if (!empty($mail) && !empty($pass)) {
-
-				$query = "SELECT password FROM users WHERE user_mail = '{$mail}'";
-
-				try{
-					$result = mysqli_query($connection, $query);
-
-					if ($result) {
-						$row = mysqli_fetch_assoc($result);
-
-						if ($row) {
-
-							if (password_verify($pass, $row["password"])) {
-
-								$_SESSION["user"] = $mail;
-								$_SESSION["password"] = $pass;
-				
-								header("Location: home.php");
-
-							}
-							else {
-								echo "Incorrect password!<br>";
-							}
-
-						}
-						else {
-							echo "User not registered!<br>";
-						}
-					}
-					else {
-						echo "Error executing query!<br>";
-					}
-				}
-				catch (mysqli_sql_exception) {
-					echo "No user with the mail {$mail} registered!<br>";
-				}
-
-			}
-
-			else {
-				echo "Please, enter a valid mail and password!";
-			}
+			processLogin();
 
 		} elseif (isset($_POST["register"])) {
 
-			$mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
-			$pass = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-
-			if (!empty($mail) && !empty($pass)) {
-
-				$user = $mail;
-
-				$hash = password_hash($pass, PASSWORD_DEFAULT);
-	
-				$query = "INSERT INTO users (user_mail, password) VALUES ('$user', '$hash')";
-	
-				try {
-					// This connection param comes from the include("./database.php");
-					// The editor can't see it hence it markes it as an error, it does work!
-					mysqli_query($connection, $query);
-					echo "User registered!<br>";
-				} catch (mysqli_sql_exception) {
-					echo "That email is already registered!";
-				}
-	
-				mysqli_close($connection);
-			}
-			else {
-				echo "Error, you need a valid mail and password to register!<br>";
-			}
+			processRegister();
 		}
+	}
+
+	function processLogin() {
+
+		global $connection;
+
+		$mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
+
+		$pass = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+		if (empty($mail) || empty($pass)) {
+			echo "Please enter a valid email and password!<br>";
+			return;
+
+		}
+
+		$query = "SELECT password FROM users WHERE user_mail = '{$mail}'";
+
+		// This connection param comes from the include("./database.php");
+		// The editor can't see it hence it markes it as an error, it does work!
+
+		$result = mysqli_query($connection, $query);
+	
+		if (!$result) {
+			echo "Error executing the query!<br>";
+
+			return;
+		}
+
+		$row = mysqli_fetch_assoc($result);
+
+		if (!$row) {
+			echo "User not registered!<br>Please register to log in!<br>";
+			return;
+		}
+
+		if (!password_verify($pass, $row["password"])) {
+			echo "Incorrect password!<br>";
+			return;
+		}
+
+		mysqli_close($connection);
+
+		$_SESSION["user"] = $mail;
+		$_SESSION["password"] = $pass;
+
+		header("Location: home.php");
+	}
+
+	function processRegister() {
+
+		global $connection;
+
+		$mail = filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL);
+
+		$pass = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+		if (empty($mail) || empty($pass)) {
+			echo "Please, enter a valid email and password!";
+			return;
+		}
+
+		$hash = password_hash($pass, PASSWORD_DEFAULT);
+
+		$query = "INSERT INTO users (user_mail, password) VALUES ('$mail', '$hash')";
+
+		try {
+			mysqli_query($connection, $query);
+
+			echo "User registered!<br>";
+		} catch (mysqli_sql_exception) {
+			echo "That email is already registered!<br>";
+
+			return;
+		}
+
+		mysqli_close($connection);
 	}
 ?>
